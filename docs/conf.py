3603fc0a13279b900451758d6d0e016d86629dc1
Aaron Wong <aaronkw@users.noreply.github.com>
2016-11-16 16:08:18 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'HumanBase'
+copyright = u'2016, Simons Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
