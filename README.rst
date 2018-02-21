c190c3102a688d3a1c8b44cbbe190776176cec1f
galiev_mr <galiev_mr@taximaxim.ru>
2018-02-21 15:50:15 +0300

    updated conf

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-Template
+pgCodeKeeper
 ========
 
 $project will solve your problem of where to start with documentation,
--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'pgCodeKeeper documentation'
+copyright = u'2014, pgCodeKeeper'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'pgCodeKeeper'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'pgCodeKeeper.tex', u'pgCodeKeeper Documentation',
+   u'pgCodeKeeper', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'pgCodeKeeper', u'pgCodeKeeper Documentation',
+     [u'pgCodeKeeper'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'pgCodeKeeper', u'pgCodeKeeper Documentation',
+   u'pgCodeKeeper', 'pgCodeKeeper', 'One line description of project.',
    'Miscellaneous'),
 ]
 
