44de8533d0147a1696242fec9a97789fec195081
Aaron Wong <aaronkw@users.noreply.github.com>
2019-01-02 16:26:55 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -98,6 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
