f7d035a11583d5b783383f61f650e15895fc8f03
aaronkw <aaronkw@gmail.com>
2019-01-02 15:55:38 -0500

    Fix logo name

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,10 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -117,11 +120,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'img/hb_logo_final.svg'
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
+html_logo = 'img/hb_full_logo.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
