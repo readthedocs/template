6ba8f79d5391dbbef1eff236813d5b9390d58f5c
Aaron Wong <aaronkw@users.noreply.github.com>
2019-01-15 10:39:58 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'HumanBase'
-copyright = u'2016, Simons Foundation'
+copyright = u'2019, Simons Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
