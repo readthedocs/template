41b85f889b2ee5d7e3bf8c21bcad3862e5f57c8f
Alex-Pennington <53480806+Alex-Pennington@users.noreply.github.com>
2019-07-30 10:35:17 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'StationMapper'
+copyright = u'2019, KyHiTech.com'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
