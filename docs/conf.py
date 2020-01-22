3ad9008db39a485eec10df32b4b83af24932ee71
Arsalan <arsalan_akhtar@outlook.com>
2020-01-22 22:04:57 +0100

    test

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
+project = u'SIESTA Documentation'
 copyright = u'2014, Read the Docs'
 
 # The version info for the project you're documenting, acts as replacement for
