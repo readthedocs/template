b1405e6c6a61018efe59cfb29099d2f6cfc18b16
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-11-22 11:50:53 -0700

    Update perms-command.js

--- docs/perms-command.js
+++ docs/perms-command.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+  name: 'add',
+  description: 'Add a new bot to the downtime check list.',
+  emoji: ':heavy_plus_sign: ',
+  requiredPermissions: PERMISSIONS.administrator,
+}
