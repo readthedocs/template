be7ee077c2b2e16b567ea10eacfaa3fce15b4344
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-03-05 16:42:11 -0700

    Delete perms-command.js

--- docs/perms-command.js
+++
@@ -1,8 +0,0 @@
-const { PERMISSIONS } = require('../common/constants');
-
-module.exports = {
-  name: 'add',
-  description: 'Add a new bot to the downtime check list.',
-  emoji: ':heavy_plus_sign: ',
-  requiredPermissions: PERMISSIONS.administrator,
-}
